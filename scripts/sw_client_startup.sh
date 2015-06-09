3bbcabacfda752c67bdf81715d2051f4ab1fba89
greg <gp_github@swimr.com>
2015-06-09 13:18:48 -0700

    none

--- scripts/sw_client_startup.sh
+++ scripts/sw_client_startup.sh
@@ -1,18 +1,4 @@
 #!/bin/bash
 
-#/System/Library/Frameworks/JavaVM.framework/Versions/A/Commands/java
-#java -Djava.security.policy=jars/policy -cp 'jars/h4.jar' applications.euclideantsp.JobEuclideanTsp djava.dyndns.org multi
-#java -Djava.security.policy=jars/policy -cp 'jars/h4.jar' applications.euclideantsp.JobEuclideanTsp localhost
-
-
 java -Djava.security.policy=scripts/jars/policy -cp 'scripts/jars/h4.jar' applications.euclideantsp.JobEuclideanTsp djava.dyndns.org
 
-# temp junk to keep the process running for a while. TESTING only.
-#counter=0
-#while [ $counter -lt 1000 ]; do
-#	echo $counter
-#	let counter=counter+1
-
-	#sleep seconds
-#	sleep 1
-#done
