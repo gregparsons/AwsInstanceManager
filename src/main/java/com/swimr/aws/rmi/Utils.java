d4cee58e4a40987b0eefcef6f34d08b74c353461
greg <gp_github@swimr.com>
2015-06-09 23:28:21 -0700

    new computer ami

--- src/main/java/com/swimr/aws/rmi/Utils.java
+++ src/main/java/com/swimr/aws/rmi/Utils.java
@@ -11,7 +11,7 @@ public class Utils {
 	public static final int MAX_EC2_INSTANCES_AT_A_TIME = 8;
 
 
-	public static final String HW_COMPUTER_AMI = "ami-35bc8405"; // 12 cities
+	public static final String HW_COMPUTER_AMI = "ami-3bbd850b"; // 12 cities
 	//public static final String HW_COMPUTER_AMI = "ami-dbb880eb"; // 10 cities
 	public static final String HW_MANAGER_AMI = "ami-8fc6febf";		// 12 cities
 	public static final InstanceType AWS_HW_MANAGER_DEFAULT_SIZE = InstanceType.M3Large;	//m3.large
